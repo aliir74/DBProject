08d5f620ea159b2f9f02fbfac92e4c4d52d8847b
Ali Irani <aliirani74@gmail.com>
2016-12-24 00:00:58 +0330

    simple Rest API added

--- Main.py
+++ Main.py
@@ -1,14 +1,24 @@
 import mysql.connector
+import _thread
 import Initialization
 import MainUpdator
 from Utilities import getInformation as info
-import Server
 
-#Initialization.create(info.getDB(info),info.getPort(info))
-Server.test()
+Initialization.create(info.getDB(info),info.getPort(info))
+
+def server( threadName ):
+    from flask import Flask
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    if __name__ == "__main__":
+        app.run()
+
+_thread.start_new_thread( server, ("Thread-1", ) )
+
 import GUI
-
-
-
 
 print ("heh")
