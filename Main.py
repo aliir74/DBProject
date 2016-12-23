8d193e77378b31aeb7617813bc0e2658527dc4ca
Ali Irani <aliirani74@gmail.com>
2016-12-23 23:01:43 +0330

    simple server added

--- Main.py
+++ Main.py
@@ -2,10 +2,10 @@ import mysql.connector
 import Initialization
 import MainUpdator
 from Utilities import getInformation as info
+import Server
 
-
-
-Initialization.create(info.getDB(info),info.getPort(info))
+#Initialization.create(info.getDB(info),info.getPort(info))
+Server.test()
 import GUI
 
 
